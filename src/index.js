e72a78547b1b2a80c7d25a3df1096b6755355ba4
BradenPKI <braden.poirier@perkinelmer.com>
2017-05-09 21:18:44 -0400

    Final Push

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import angular from 'angular';
-import {ngAnimate} from 'angular-animate';
-import {ngSanitize} from 'angular-sanitize';
+import { ngAnimate } from 'angular-animate';
+import { ngSanitize } from 'angular-sanitize';
 import 'angular-ui-bootstrap';
 
 import 'angular-ui-router';
+import 'angular-translate';
 import routesConfig from './routes';
 
-import {hello} from './app/hello';
-import {test01} from './test01/app';
-import {test02} from './test02/app';
+import { hello } from './app/hello';
+
+import { buttonService } from './test01/services/buttonService';
+import { windowResizeDirective } from './test01/directives/windowResize';
+import { buttonResizeDirective } from './test01/directives/buttonResize';
+import { test01 } from './test01/app';
+import { test02 } from './test02/app';
 
 import './index.scss';
 
 export const app = 'app';
 
 angular
-  .module(app, ['ngAnimate', 'ngSanitize','ui.router', 'ui.bootstrap'])
+  .module(app, ['ngAnimate', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'pascalprecht.translate'])
   .config(routesConfig)
   .component('app', hello)
   .component('test01', test01)
   .component('test02', test02);
+
+buttonService();
+windowResizeDirective();
+buttonResizeDirective();
+
